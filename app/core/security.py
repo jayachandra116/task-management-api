3bc5113302b935ff3e2a7c9322af81d45409ea53
Jaya Chandra M <mjchandra97@gmail.com>
2026-05-06 16:51:50 +0530

    refactor(api): add default value for token expiration minutes

--- app/core/security.py
+++ app/core/security.py
@@ -9,7 +9,7 @@ pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
 SECRET_KEY = os.getenv("SECRET_KEY")
 ALGORITHM = os.getenv("ALGORITHM")
-ACCESS_TOKEN_EXPIRE_MINUTES = int(os.getenv("ACCESS_TOKEN_EXPIRE_MINUTES"))
+ACCESS_TOKEN_EXPIRE_MINUTES = int(os.getenv("ACCESS_TOKEN_EXPIRE_MINUTES", 30))
 
 
 def verify_password(plain_password, hashed_password):
