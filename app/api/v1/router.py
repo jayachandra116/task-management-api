6f72a46c12734681499351517a11398560846995
Jaya Chandra M <mjchandra97@gmail.com>
2026-05-06 16:53:47 +0530

    feat(api): update and fix the url for api versioning

--- app/api/v1/router.py
+++ app/api/v1/router.py
@@ -1,7 +1,7 @@
 from fastapi import APIRouter
 from app.api.v1.routers import auth, user, task
 
-router = APIRouter(prefix="/app/v1")
+router = APIRouter(prefix="/api/v1")
 
 router.include_router(auth.router)
 router.include_router(user.router)
