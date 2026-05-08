bfa5a6d36e18b1db1ac786231e2a3ffd82a20de1
Jaya Chandra M <mjchandra97@gmail.com>
2026-05-08 23:43:29 +0530

    docs(users): add FastAPI metadata and Swagger UI description for users

--- app/api/v1/routers/user.py
+++ app/api/v1/routers/user.py
@@ -22,12 +22,25 @@ db_dependency = Annotated[Session, Depends(get_db)]
 user_dependency = Annotated[User, Depends(get_current_user)]
 
 
-@router.get("/me", status_code=status.HTTP_200_OK, response_model=UserResponse)
+@router.get(
+    "/me",
+    status_code=status.HTTP_200_OK,
+    response_model=UserResponse,
+    tags=["User"],
+    summary="Get the current user",
+    description="This returns the currently logged in user details",
+)
 def get_me(current_user: user_dependency):
     return current_user
 
 
-@router.patch("/me/password", status_code=status.HTTP_204_NO_CONTENT)
+@router.patch(
+    "/me/password",
+    status_code=status.HTTP_204_NO_CONTENT,
+    tags=["User Management"],
+    summary="Change the user password",
+    description="Change the current user's password",
+)
 async def change_current_user_password(
     payload: PasswordChange, db: db_dependency, current_user: user_dependency
 ):
@@ -37,7 +50,13 @@ async def change_current_user_password(
 
 
 # For admin
-@router.get("/", response_model=UserPaginatedResponse)
+@router.get(
+    "/",
+    response_model=UserPaginatedResponse,
+    tags=["Admin"],
+    summary="List all users",
+    description="List all the users for admin users",
+)
 def list_users(
     db: db_dependency,
     user: User = Depends(require_admin),
@@ -47,7 +66,13 @@ def list_users(
     return admin.list_all_users(db, user, page, size)
 
 
-@router.get("/{user_id}", response_model=UserResponse)
+@router.get(
+    "/{user_id}",
+    response_model=UserResponse,
+    tags=["User"],
+    summary="Get the user details",
+    description="Get the user details to the admin users",
+)
 def get_user(
     user_id: int,
     db: db_dependency,
@@ -56,7 +81,13 @@ def get_user(
     return admin.get_user(user_id, db, user)
 
 
-@router.patch("/{user_id}/role", response_model=UserResponse)
+@router.patch(
+    "/{user_id}/role",
+    response_model=UserResponse,
+    tags=["User"],
+    summary="Update the user",
+    description="Update the given user by admin users",
+)
 def update_user_role(
     user_id: int,
     payload: UserRoleUpdate,
@@ -66,7 +97,13 @@ def update_user_role(
     return admin.update_user_role(user_id, payload, db, current_user)
 
 
-@router.delete("/{user_id}", status_code=status.HTTP_204_NO_CONTENT)
+@router.delete(
+    "/{user_id}",
+    status_code=status.HTTP_204_NO_CONTENT,
+    tags=["User"],
+    summary="Delete a user",
+    description="Delete the given user by admin users",
+)
 def delete_user(
     user_id: int,
     db: Session = Depends(get_db),
